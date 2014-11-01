106b211b85ab74b58858e4d32bc64799f0b6c815
Gajus Kuizinas <gk@anuary.com>
2014-11-01 14:21:41 +0200

    Reimplemented the resource resolution with max response size support.

--- src/deadlink.js
+++ src/deadlink.js
@@ -2,45 +2,25 @@ var Deadlink = {},
     Promise = require('bluebird'),
     http = require('http'),
     url = require('url'),
-    jsdom = require('jsdom');
+    jsdom = require('jsdom'),
+    mmm = require('mmmagic'),
+    Magic = mmm.Magic;
 
 Deadlink = function () {
     var deadlink = {},
-        requestedUrls = {};
-
-    Deadlink.Resolution = function (error, result) {
-        this.error = error;
-        this.result = result;
-    };
+        requestedUrls = {}; 
 
     /**
-     * Promise is resolved if the URL is resolved.
-     * Promise is rejected if the status code is 404 or
-     * the content-type is not text.
-     * 
+     * Memoization of Deadlink.resolveURL.
+     *
      * @param {String} subjectURL
      * @param {Promise}
      */
     deadlink.resolveURL = function (subjectURL) {
-        // Treat http://foo.com/ and http://foo.com/#resource-identifier
-        // as the same when making a request and looking of the cache.
-        subjectURL = url.parse(subjectURL);
-        delete subjectURL.hash;
-        subjectURL = url.format(subjectURL);
+        subjectURL = Deadlink.normaliseURL(subjectURL);
 
         if (requestedUrls[subjectURL] === undefined) {
-            requestedUrls[subjectURL] = new Promise(function (resolve, reject) {
-                http.get(subjectURL, function (response) {
-                    if (response.statusCode == 404) {
-                        resolve(new Deadlink.Resolution('Resource not found.'));
-                    } else {
-                        response.setEncoding('utf8');
-                        response.on('data', function (data) {
-                            resolve(new Deadlink.Resolution(null, data));
-                        });
-                    }
-                }).on('error', reject);
-            });
+            requestedUrls[subjectURL] = Deadlink.resolveURL(subjectURL);
         }
 
         return requestedUrls[subjectURL];
@@ -57,6 +37,104 @@ Deadlink = function () {
     };
 
     return deadlink;
+};
+
+Deadlink.Resolution = function (data) {
+    var resolution = this;
+    Object.keys(data).forEach(function (k) {
+        resolution[k] = data[k];
+    });
+};
+
+/**
+ * Treat http://foo.com/ and http://foo.com/#resource-identifier
+ * as the same when making a request and looking of the cache.
+ *
+ * @param {String} subjectURL
+ * @return {String}
+ */
+Deadlink.normaliseURL = function (subjectURL) {
+    subjectURL = url.parse(subjectURL);
+    delete subjectURL.hash;
+    return url.format(subjectURL);
+};
+
+/**
+ * Interpret HTTP response and respond to promise.
+ * 
+ * Resolution is an instance of Deadlink.Resolution with
+ * the following properties:
+ * 
+ * .error, when status code is >= 400.
+ * .statusCode, when status code is >= 400
+ * .url
+ * .body, when response is HTML document.
+ * .contentType, when response is other than HTML document. 
+ * 
+ * @param {String} subjectURL
+ * @param {Promise}
+ */
+Deadlink.resolveURL = function (subjectURL) {
+    return new Promise(function (resolve, reject) {
+        http.get(subjectURL, function (response) {
+            var request = this,
+                magic,
+                responseData = '',
+                respondeContentType = response.headers['content-type'];
+
+            if (response.statusCode >= 400) {
+                return resolve(
+                    new Deadlink.Resolution({
+                        error: 'Resource not resolvable.',
+                        url: subjectURL,
+                        statusCode: response.statusCode
+                    })
+                );
+            }
+
+            if (respondeContentType.toLowerCase().indexOf('text/html') !== 0) {
+                return resolve(new Deadlink.Resolution({
+                    url: subjectURL,
+                    contentType: respondeContentType
+                }));
+            }
+
+            response.on('data', function (chunk) {
+                responseData += chunk;
+
+                // 100 * 1000 max buffer size, 100kb
+                if (responseData.length > 100 * 1000) {
+                    request.abort();
+
+                    return resolve(
+                        new Deadlink.Resolution({
+                            error: 'Resource is larger than 100kb.',
+                            url: subjectURL
+                        })
+                    );
+                }
+
+                magic = new Magic(false, mmm.MAGIC_MIME_TYPE);
+
+                // Make sure that what we are reading is HTML data.
+                magic.detect(chunk, function (err, result) {
+                    if (result.toLowerCase().indexOf('text/') !== 0) {
+                        request.abort();
+
+                        return reject(new Error('Resource is not text.'));
+                    }
+                });
+            });
+
+            response.on('end', function () {
+                resolve(new Deadlink.Resolution({
+                    url: subjectURL,
+                    contentType: respondeContentType,
+                    body: responseData
+                }));
+            });
+        }).on('error', reject);
+    });
 };
 
 module.exports = Deadlink;
--- tests/deadlink.js
+++ tests/deadlink.js
@@ -5,78 +5,76 @@ var chai = require('chai'),
     Sinon = require('sinon'),
     Promise = require('bluebird');
 
-//require('mocha-as-promised')();
-
 chai.use(chaiAsPromised);
 
 describe('deadlink', function () {
-    var Deadlink,
-        deadlink,
-        sinon;
+    var Deadlink;
     beforeEach(function () {
         Deadlink = require('../src/deadlink.js');
-        deadlink = Deadlink();
-        sinon = Sinon.sandbox.create();
-    });
-    afterEach(function () {
-        sinon.restore();
     });
     describe('.resolveURL(url)', function () {
         it('promise is resolved with a Deadlink.Resolution', function () {
-            nock('http://gajus.com').get('/').reply(200, 'OK');
-            return deadlink
+            nock('http://gajus.com').get('/').reply(200, 'OK', {'content-type': 'text/html'});
+            return Deadlink
                 .resolveURL('http://gajus.com')
                 .then(function (Resolution) {
                     expect(Resolution).to.instanceof(Deadlink.Resolution);
                 });
+                
         });
-        describe('Deadlink.Resolution of successful resource resolution', function () {
-            it('has no error', function () {
-                nock('http://gajus.com').get('/').reply(200, 'OK');
-                    return deadlink
-                        .resolveURL('http://gajus.com')
-                        .then(function (Resolution) {
-                            expect(Resolution.error).to.null;
-                        });
-            });
-            it('has body of the resolved resource', function () {
-                nock('http://gajus.com').get('/').reply(200, 'OK');
-                    return deadlink
-                        .resolveURL('http://gajus.com')
-                        .then(function (Resolution) {
-                            expect(Resolution.result).to.equal('OK');
-                        });
-            });
-        });
-        describe('Deadlink.Resolution of unsuccessful resource resolution', function () {
-            it('has error', function () {
-                nock('http://gajus.com').get('/').reply(404);
-                    return deadlink
-                        .resolveURL('http://gajus.com')
-                        .then(function (Resolution) {
-                            expect(Resolution.error).to.equal('Resource not found.');
-                        });
-            });
-        });
-    });
-    describe('.resolveURLs(urls)', function () {
-        it('promise is resolved with a Deadlink.Resolution collection', function () {
-            var spy = sinon.spy(deadlink, 'resolveURL');
-
-            nock('http://gajus.com').get('/found-1').reply(200, 'OK');
-            nock('http://gajus.com').get('/found-2').reply(200, 'OK');
-            nock('http://gajus.com').get('/found-3').reply(200, 'OK');
-
-            return deadlink
-                .resolveURLs(['http://gajus.com/found-1', 'http://gajus.com/found-2', 'http://gajus.com/found-3'])
-                .then(function (Resolutions) {
-                    expect(spy.calledWith('http://gajus.com/found-1')).to.true;
-                    expect(spy.calledWith('http://gajus.com/found-2')).to.true;
-                    expect(spy.calledWith('http://gajus.com/found-3')).to.true;
-                    expect(spy.callCount).to.equal(3);
-                    expect(Resolutions.length).to.equal(3);
-                    expect(Resolutions[0]).to.instanceof(Deadlink.Resolution);
+        describe('Deadlink.Resolution', function () {
+            describe('resolution of HTML resource', function () {
+                var promise;
+                beforeEach(function () {
+                    nock('http://gajus.com').get('/').reply(200, 'OK', {'content-type': 'text/html'});
+                    promise = Deadlink
+                        .resolveURL('http://gajus.com/');
                 });
+                it('has URL and response body', function () {
+                    return promise.then(function (Resolution) {
+                        expect(Resolution).to.deep.equal({url: 'http://gajus.com/', contentType: 'text/html', body: 'OK'});
+                    });
+                });
+            });
+            describe('resolution of resource other than HTML', function () {
+                var promise;
+                beforeEach(function () {
+                    nock('http://gajus.com').get('/').reply(200, 'OK', {'content-type': 'application/json'});
+                    promise = Deadlink
+                        .resolveURL('http://gajus.com/');
+                });
+                it('has URL and content type', function () {
+                    return promise.then(function (Resolution) {
+                        expect(Resolution).to.deep.equal({url: 'http://gajus.com/', contentType: 'application/json'});
+                    });
+                });
+            });
+            describe('resolution of status code >=400', function () {
+                var promise;
+                beforeEach(function () {
+                    nock('http://gajus.com').get('/').reply(404, 'OK', {'content-type': 'text/html'});
+                    promise = Deadlink
+                        .resolveURL('http://gajus.com/');
+                });
+                it('has URL, error and status code', function () {
+                    return promise.then(function (Resolution) {
+                        expect(Resolution).to.deep.equal({url: 'http://gajus.com/', error: 'Resource not resolvable.', statusCode: 404});
+                    });
+                });
+            });
+            describe('resolution of HTML resource larger than 100kb', function () {
+                var promise;
+                beforeEach(function () {
+                    nock('http://gajus.com').get('/').reply(200, Array(100 * 1000 + 100).join('X'), {'content-type': 'text/html'});
+                    promise = Deadlink
+                        .resolveURL('http://gajus.com/');
+                });
+                it('has URL and error', function () {
+                    return promise.then(function (Resolution) {
+                        expect(Resolution).to.deep.equal({url: 'http://gajus.com/', error: 'Resource is larger than 100kb.'});
+                    });
+                });
+            });
         });
     });
 });
